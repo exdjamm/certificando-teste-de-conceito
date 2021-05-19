d359d8b3bad75e0edea627102c6baf44a556de59
exodojaffar <exodojaffar@gmail.com>
2021-05-18 20:55:55 -0400

    feat: adiciona bagulhos de tags

--- canvas_box.js
+++ canvas_box.js
@@ -1,17 +1,16 @@
+const px = 3;
+
 const canvas = document.querySelector("#canvas")
 let  offSet = canvas.getBoundingClientRect()
 
 let ctx = canvas.getContext('2d');
 ctx.strokeStyle = '#000';
-ctx.lineWidth = 4;
+ctx.lineWidth = px;
 
 // Values
 let edit = false;
-let i = 0;
-let selections = [{
-	initPos: {x:0, y:0},
-	endPos: {x:0, y:0}
-}]
+let i = "";
+let selections = {}
 
 let data = selections[i]
 
@@ -25,23 +24,18 @@ const newSelection = () => {
 
 const setNew = (value) => {
 	edit = true
+	// data = newSelection()
 }
 
 const setNewEnd = (value) => {
 	edit = false
-
-	selections.push(newSelection())
-	i = selections.length - 1
-
-
-	data = selections[i]
 }
 
 function updateDraw() {
 	if(edit){
 		ctx.clearRect(0, 0, canvas.width, canvas.height)
 		// desenharFoto()
-		selections.forEach((data)=> {
+		Object.values(selections).forEach((data)=> {
 			ctx.strokeRect(data.initPos.x, data.initPos.y, data.endPos.x, data.endPos.y)
 		})
 		
@@ -50,7 +44,16 @@ function updateDraw() {
 
 
 const updateInit = (e) => {
+	
+	if(Object.values(selections).length == 0){
+		throw Error("Nenhum campo criado");
+	}
+
 	setNew()
+
+	
+
+	data.endPos = {x:0, y: 0}
 
 	const {clientX, clientY} = e 
 
@@ -60,6 +63,10 @@ const updateInit = (e) => {
 }
 
 const updateMove = (e) => {
+
+	if(Object.values(selections).length == 0){
+		throw Error("Nenhum campo criado");
+	}
 
 	const {clientX, clientY} = e 
 
@@ -89,7 +96,7 @@ function desenharFoto() {
 			offSet = canvas.getBoundingClientRect()
 
 			ctx.strokeStyle = '#000';
-			ctx.lineWidth = 4;
+			ctx.lineWidth = px;
 
 		}, 0)
 
--- info_certi.js
+++ info_certi.js
@@ -7,6 +7,15 @@ const updateButtons = (value) => {
 	newButton.innerText = value
 
 	newButton.className = "button"
+	newButton.addEventListener('click', () => {
+		i = value
+
+		selections[value] = newSelection()
+
+		data = selections[value]
+
+		selections['objetc_1'] = {initPos: {x:0, y:0},endPos: {x:0, y:0}}
+	})
 
 	document.querySelector("#buttons").appendChild(newButton)
 
